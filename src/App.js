94f759420737d2c5bb39f0cf03d3f34259d63a90
facuG03 <facundogonzales1550@gmail.com>
2022-09-11 17:08:14 -0300

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Main from './components/Main'
 import Nav from './components/Nav'
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
 
 function App () {
   return (
     <>
-      <Nav />
-      <Main />
+      <Route exact path='/'>
+        <Nav />
+        <Main />
+      </Route>
+      <Route path='/create'>
+        <Link to='/'>
+          <p>atras</p>
+        </Link>
+        <p>hola</p>
+      </Route>
     </>
   )
 }
