3d8d2e8ed036c6ade6057df81f0d6a3f439aab8d
facuG03 <facundogonzales1550@gmail.com>
2022-09-11 07:28:54 -0300

    Nav component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Main from './components/Main'
+import Nav from './components/Nav'
+import React from 'react'
+
+function App () {
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,37 @@
+import styles from './Nav.module.css'
+import img from '../img/poke-ball.png'
+
+export default function Nav () {
+  return (
+    <nav className={styles.nav}>
+
+      <div className={styles.logo}>
+        <img className={styles.image} src={img} alt='pokemon' />
+        <h1 className={styles.title}>Pokemon</h1>
+      </div>
+
+      <div>
+        <input placeholder='Search a Pokemon' className={styles.search} type='text' />
+        <input type='button' className={styles.searchicon} />
+      </div>
+
+      <div className={styles.create}>
+        <div className={styles.camera}>
+          <div className={styles.ligth} />
+        </div>
+
+        <div className={styles.buttoncolor}>
+          <div className={styles.colors}>
+            <div className={styles.red} />
+            <div className={styles.yellow} />
+            <div className={styles.green} />
+          </div>
+          <div>
+            <button className={styles.createpoke}>Create Pokemon</button>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
