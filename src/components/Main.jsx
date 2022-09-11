84960e3559b93da5eb5a08d58d30195c820318d9
facuG03 <facundogonzales1550@gmail.com>
2022-09-11 17:08:33 -0300

    Minor fixes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,42 +14,28 @@ export default function Main () {
         return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif', id: res.id.toString() }
       }),
       fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
+        return { name: res.name, type: ` ${res.types[0].type.name}/${res.types[1].type.name}`, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
       }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
+      fetch('https://pokeapi.co/api/v2/pokemon/4/').then(res => res.json()).then(res => {
+        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif', id: res.id.toString() }
       }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
+      fetch('https://pokeapi.co/api/v2/pokemon/5/').then(res => res.json()).then(res => {
+        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif', id: res.id.toString() }
       }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
-      }), fetch('https://pokeapi.co/api/v2/pokemon/1/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif', id: res.id.toString() }
-      }),
-      fetch('https://pokeapi.co/api/v2/pokemon/2/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif', id: res.id.toString() }
-      }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
-      }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
-      }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
-      }),
-      fetch('https://pokeapi.co/api/v2/pokemon/3/').then(res => res.json()).then(res => {
-        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif', id: res.id.toString() }
+      fetch('https://pokeapi.co/api/v2/pokemon/6/').then(res => res.json()).then(res => {
+        return { name: res.name, type: res.types[0].type.name, img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif', id: res.id.toString() }
       })
     ]).then(res => setPokemons(res))
   }, [])
 
-  console.log(pokemons)
-
   return (
     <div className={styles.main}>
-      <section className={styles.filtro}>izquierda</section>
+      <section className={styles.filtro}>
+        <h2 className={styles.filtertitle}>Filter by</h2>
+        <h4>Type</h4>
+        <h4>Order</h4>
+        <h4>Origin</h4>
+      </section>
       <div className={styles.mid} />
       <section className={styles.card}>
         {pokemons
@@ -60,7 +46,7 @@ export default function Main () {
               type={poke.type}
               img={poke.img}
                                  />)
-          : <h2>no hay</h2>}
+          : <h2 style={{ fontFamily: 'RobotoMono' }}>Pokemon not found</h2>}
       </section>
     </div>
   )
