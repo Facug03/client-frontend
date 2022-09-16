4fe9feb4cc4c6ca5accead80f7f14038b5b6b2db
facuG03 <facundogonzales1550@gmail.com>
2022-09-16 17:31:43 -0300

    modal for pokemons

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
+import Pokemon from './Pokemon'
 import styles from './PokeCard.module.css'
+import { typeColor, typeText } from '../helpers/typeColor'
 
-export default function PokeCard ({ id, name, type, img }) {
+export default function PokeCard ({ id, name, types }) {
+  const [modal, setModal] = useState(false)
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`
   let idPoke = ''
-  if (id.length === 1) {
+
+  if (id.toString().length === 1) {
     idPoke = `N.º 00${id}`
-  } else if (id.length === 2) {
+  } else if (id.toString().length === 2) {
     idPoke = `N.º 0${id}`
   } else {
     idPoke = `N.º ${id}`
@@ -15,7 +21,7 @@ export default function PokeCard ({ id, name, type, img }) {
       <div className={styles.pokecard}>
         <div className={styles.detailright} />
         <div className={styles.detailleft} />
-        <img className={styles.imagepoke} src={img} alt='pokemon' />
+        <img onClick={() => setModal(true)} className={styles.imagepoke} src={img} alt='pokemon' />
       </div>
       <div className={styles.middledetail}>
         <hr />
@@ -23,10 +29,19 @@ export default function PokeCard ({ id, name, type, img }) {
         <hr />
       </div>
       <div className={styles.data}>
-        <span className={styles.idpoke}>{idPoke}</span>
-        <h3 className={styles.namepoke}>{name}</h3>
-        <span className={styles.type}>{type}</span>
+        <span onClick={() => setModal(true)} className={styles.idpoke}>{idPoke}</span>
+        <h3 onClick={() => setModal(true)} className={styles.namepoke}>{name}</h3>
+        <span style={{ backgroundColor: typeColor(types[0]), color: typeText(types[0]) }} className={styles.type}>{types[0]}</span>{types[1] && <span style={{ backgroundColor: typeColor(types[1]), color: typeText(types[1]) }} className={styles.type2}>{types[1]}</span>}
       </div>
+      <Pokemon
+        color={typeColor(types[0])}
+        id={id}
+        idPoke={idPoke}
+        name={name}
+        img={img}
+        modal={modal}
+        setModal={setModal}
+      />
     </div>
   )
 }
