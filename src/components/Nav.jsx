7c91b8122781269d33a8b6515875d358d81fec86
facuG03 <facundogonzales1550@gmail.com>
2022-09-16 17:31:27 -0300

    change logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,26 @@
+/* eslint-disable react/jsx-closing-tag-location */
+import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 import styles from './Nav.module.css'
 import img from '../img/poke-ball.png'
-import { Link } from 'react-router-dom'
+import logo from '../img/Pokedex.png'
 
 export default function Nav () {
+  const location = useLocation()
+  const [current, setCurrent] = useState(false)
+  if (location.pathname === '/create' && !current) {
+    setCurrent(true)
+  } else if (location.pathname === '/' && current) {
+    setCurrent(false)
+  }
   return (
     <nav className={styles.nav}>
-
-      <div className={styles.logo}>
-        <img className={styles.image} src={img} alt='pokemon' />
-        <h1 className={styles.title}>Pokemon</h1>
-      </div>
-
+      <Link to='/'>
+        <div className={styles.logo}>
+          <img className={styles.image} src={img} alt='pokemon' />
+          <img className={styles.title} src={logo} alt='logo' />
+        </div>
+      </Link>
       <div>
         <input placeholder='Search Pokemon...' className={styles.search} type='text' />
         <input type='button' className={styles.searchicon} />
@@ -22,18 +32,19 @@ export default function Nav () {
         </div>
 
         <div className={styles.buttoncolor}>
-          <div className={styles.colors}>
+          <div className={!current ? styles.colors : styles.current}>
             <div className={styles.red} />
             <div className={styles.yellow} />
             <div className={styles.green} />
           </div>
-          <div>
-            <Link to='/create'>
-              <button className={styles.createpoke}>Create Pokemon</button>
-            </Link>
-          </div>
+          {!current
+            ? <section>
+              <Link to='/create'>
+                <button className={styles.createpoke}>Create Pokemon</button>
+              </Link>
+            </section>
+            : null}
         </div>
-
       </div>
     </nav>
   )
