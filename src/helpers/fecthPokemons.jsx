ce76b3474a28009588449c15e6ddf06ab13a1520
facuG03 <facundogonzales1550@gmail.com>
2022-09-20 19:54:48 -0300

    added stats to pokemons

--- src/helpers/fecthPokemons.jsx
+++ src/helpers/fecthPokemons.jsx
@@ -6,15 +6,27 @@ export const fetchPokemons = async () => {
   return transformSmallPokemonIntoPokemon(smallPokemonList)
 }
 
-const transformSmallPokemonIntoPokemon = async (smallPokemonList) => {
-  const pokemonArr = await Promise.all(smallPokemonList.map(poke => axios.get(`https://pokeapi.co/api/v2/pokemon/${poke.url.split('/')[6]}`)
+const transformSmallPokemonIntoPokemon = (smallPokemonList) => {
+  const pokemonArr = Promise.all(smallPokemonList.map(poke => axios.get(`https://pokeapi.co/api/v2/pokemon/${poke.url.split('/')[6]}`)
     .then(({ data }) => {
       const id = data.id
       const types = data.types.map(types => types.type.name)
+      const hp = data.stats[0].base_stat
+      const attack = data.stats[1].base_stat
+      const defense = data.stats[2].base_stat
+      const specialAt = data.stats[3].base_stat
+      const specialDef = data.stats[4].base_stat
+      const speed = data.stats[5].base_stat
       return {
         id,
         name: data.name,
-        types
+        types,
+        hp,
+        attack,
+        defense,
+        specialAt,
+        specialDef,
+        speed
       }
     }))).then(res => res)
   return pokemonArr
