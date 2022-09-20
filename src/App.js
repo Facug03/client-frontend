5b0be795c6001e30b94c9f564da09453b894c4cb
facuG03 <facundogonzales1550@gmail.com>
2022-09-20 19:52:41 -0300

    custom hook added to app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Pokemon from './components/Pokemon'
 import Create from './components/Create'
 import Footer from './components/Footer'
 import { Route } from 'react-router-dom'
+import { usePokemon } from './hooks/usePokemon'
 
 function App () {
+  usePokemon()
+
   return (
     <>
       <Nav />
