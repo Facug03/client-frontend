e7a24cac0c626f6bdbc0bc74b111bc8c8b185ff7
facuG03 <facundogonzales1550@gmail.com>
2022-09-20 19:55:46 -0300

    styles for not found pokemons

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -2,8 +2,20 @@ import { useState } from 'react'
 import Pokemon from './Pokemon'
 import styles from './PokeCard.module.css'
 import { typeColor, typeText } from '../helpers/typeColor'
+import image from '../img/not-found.png'
 
-export default function PokeCard ({ id, name, types }) {
+export default function PokeCard ({
+  id,
+  name,
+  types,
+  filter,
+  hp,
+  attack,
+  defense,
+  specialAt,
+  specialDef,
+  speed
+}) {
   const [modal, setModal] = useState(false)
   const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`
   let idPoke = ''
@@ -16,22 +28,38 @@ export default function PokeCard ({ id, name, types }) {
     idPoke = `N.º ${id}`
   }
 
+  if (filter) {
+    return (
+      <div className={styles.notfound}>
+        <img className={styles.imfound} src={image} alt='not-found' />
+        <div className={styles.notfoundtitle}>
+          {filter}
+          <ul className={styles.listul}>
+            <li className={styles.list}>Look if you wrote it rigth</li>
+            <li className={styles.list}>Try with another word</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div className={styles.cardcontainer}>
-      <div className={styles.pokecard}>
-        <div className={styles.detailright} />
-        <div className={styles.detailleft} />
-        <img onClick={() => setModal(true)} className={styles.imagepoke} src={img} alt='pokemon' />
-      </div>
-      <div className={styles.middledetail}>
-        <hr />
-        <hr />
-        <hr />
-      </div>
-      <div className={styles.data}>
-        <span onClick={() => setModal(true)} className={styles.idpoke}>{idPoke}</span>
-        <h3 onClick={() => setModal(true)} className={styles.namepoke}>{name}</h3>
-        <span style={{ backgroundColor: typeColor(types[0]), color: typeText(types[0]) }} className={styles.type}>{types[0]}</span>{types[1] && <span style={{ backgroundColor: typeColor(types[1]), color: typeText(types[1]) }} className={styles.type2}>{types[1]}</span>}
+    <>
+      <div className={styles.cardcontainer}>
+        <div className={styles.pokecard}>
+          <div className={styles.detailright} />
+          <div className={styles.detailleft} />
+          <img onClick={() => setModal(true)} className={styles.imagepoke} src={img} alt='pokemon' />
+        </div>
+        <div className={styles.middledetail}>
+          <hr />
+          <hr />
+          <hr />
+        </div>
+        <div className={styles.data}>
+          <span onClick={() => setModal(true)} className={styles.idpoke}>{idPoke}</span>
+          <h3 onClick={() => setModal(true)} className={styles.namepoke}>{name}</h3>
+          <span style={{ backgroundColor: typeColor(types[0]), color: typeText(types[0]) }} className={styles.type}>{types[0]}</span>{types[1] && <span style={{ backgroundColor: typeColor(types[1]), color: typeText(types[1]) }} className={styles.type2}>{types[1]}</span>}
+        </div>
       </div>
       <Pokemon
         color={typeColor(types[0])}
@@ -41,7 +69,13 @@ export default function PokeCard ({ id, name, types }) {
         img={img}
         modal={modal}
         setModal={setModal}
+        hp={hp}
+        attack={attack}
+        defense={defense}
+        specialAt={specialAt}
+        specialDef={specialDef}
+        speed={speed}
       />
-    </div>
+    </>
   )
 }
